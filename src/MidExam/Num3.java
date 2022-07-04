cec837209b5a9ac5cae4e0c71281d2530e3c9d16
Kristiyan Kolev <kolevkristian1@gmail.com>
2022-07-04 16:53:07 +0300

    Name Changes
    Changes to the names of the files

--- src/MidExam/Num3.java
+++
@@ -1,9 +0,0 @@
-package MidExam;
-
-import java.util.Scanner;
-
-public class Num3 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}
--- src/MidExam/SchoolLibrary.java
+++ src/MidExam/SchoolLibrary.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class N03 {
+public class SchoolLibrary {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
